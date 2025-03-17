f7de523d29d60ea1df9671c468379bb94a7b50ac
KanishkaArochana <kanishkaarochana23@gmail.com>
2025-03-17 21:28:49 +0530

    Create main function

--- src/Billing_Print_System.java
+++ src/Billing_Print_System.java
@@ -842,3 +842,21 @@ public class Billing_Print_System extends javax.swing.JFrame {
         jTextPane1.setText(jTextPane1.getText() + "Date: " + date + "\t\t  Time: " + time + " \n");
 
     }
+
+      
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
